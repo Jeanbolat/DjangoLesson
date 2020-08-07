739aa6b8b71d5b862759f89ac3a9ad291386958d
Jeanbolat <63873712+Jeanbolat@users.noreply.github.com>
2020-08-07 20:41:19 +0600

    some changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,4 @@
-
+import os
 
 from pathlib import Path
 
@@ -109,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
